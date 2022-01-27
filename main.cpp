58fe1eed56e0a286a30de3af9043f883ad501f2c
NP199 <niklas.pierick@stud.thga.de>
2022-01-27 17:44:10 +0100

    add new Button, Spinbox

--- main.cpp
+++ main.cpp
@@ -27,13 +27,27 @@ int main(int argc, char *argv[])
    QPushButton *pb1 = new QPushButton();
    pb1->setText("Close");
 
+   QPushButton *pb2 = new QPushButton();
+   pb2->setText("set");
+
+
    QProgressBar *progress1 = new QProgressBar();
+
+   QSpinBox *spinbox1 = new QSpinBox();
+   spinbox1->setValue(0);
+   spinbox1->setRange(0,3);
+
+   QCheckBox *checkbox1 = new QCheckBox();
    
+
+   QLineEdit *lineedit1 = new QLineEdit();
+  
    QSlider *slider1 = new QSlider();
    slider1->setOrientation(Qt::Orientation(0x1));
    slider1->setRange(0,100);
+   slider1->setTickPosition(QSlider::NoTicks);
 
-
+   
    QHBoxLayout *layout1 = new QHBoxLayout();
    layout1->addStretch();
    layout1->addWidget(progress1);
@@ -43,19 +57,65 @@ int main(int argc, char *argv[])
 
    QHBoxLayout *layout2 = new QHBoxLayout();
    layout2->addStretch();
-   layout2->addWidget(pb1);
+   layout2->addWidget(pb2);
+   layout2->addSpacing(10);
+   layout2->addWidget(spinbox1);
+   layout2->addSpacing(10);
+   layout2->addWidget(checkbox1);
+   layout2->addSpacing(10);
+   layout2->addWidget(lineedit1);
    layout2->addStretch();
+
+   QHBoxLayout *layout3 = new QHBoxLayout();
+   layout3->addStretch();
+   layout3->addWidget(pb1);
+   layout3->addStretch();
 
    QVBoxLayout *layoutMain = new QVBoxLayout(mainWindow);
    layoutMain->addLayout(layout1);
-   layoutMain->addSpacing(10);
+   layoutMain->addSpacing(25);
    layoutMain->addLayout(layout2);
+   layoutMain->addSpacing(50);
+   layoutMain->addLayout(layout3);
 
-   QObject::connect(slider1, QAbstractSlider::valueChanged, progress1, QProgressBar::setValue);
+
+   QObject::connect(slider1, &QAbstractSlider::valueChanged, progress1, &QProgressBar::setValue);
    QObject::connect(pb1, &QPushButton::clicked, mainWindow, &QWidget::close);
+   QObject::connect(spinbox1, &QAbstractSpinBox::editingFinished, slider1, [slider1,spinbox1,checkbox1](){
+           int temp = spinbox1->value();
+           if(checkbox1->checkState() == 2)
+           {
+           slider1->setTickPosition(QSlider::TickPosition(temp));
+           }
+   } );
+   QObject::connect(checkbox1, &QCheckBox::stateChanged, slider1, [slider1,spinbox1,checkbox1](){
+        if(checkbox1->checkState() == 2)
+        {
+            int temp = spinbox1->value();
+            slider1->setTickPosition(QSlider::TickPosition(temp));
+        }
+        else
+        {
+            slider1->setTickPosition(QSlider::NoTicks);
+         }
+      } );
+   QObject::connect(pb2, &QPushButton::clicked, lineedit1, [lineedit1,progress1](){
+      int temp = progress1->value();	
+      if(temp == 100)
+      {
+         lineedit1->setText("100% YAY!!11!11!");
+      }
+      else
+      {
+         lineedit1->setText("100% not reached yet");
+      }
+   });
+   
 
    mainWindow->show();
 
+
    return app.exec();
 }
+
 
